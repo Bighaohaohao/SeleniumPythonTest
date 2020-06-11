57fd96dc950ba3137f9bfc6af7c893e6eefd8c28
Bighaohaohao <2358418709@qq.com>
2020-06-11 17:46:15 +0800

    提交

--- find_element_test.py
+++ find_element_test.py
@@ -9,7 +9,22 @@
 from selenium import webdriver
 import time
 driver = webdriver.Chrome()
-driver.get('https://www.imooc.com/course/list')
-time.sleep(5)
-driver.find_element_by_partial_link_text('用技术打造小程序简历，让面试官耳目一新').click()
+# 使用.find_element_by_partial_link_text 定位超链接下的元素
+# driver.get('https://www.imooc.com/course/list')
+# time.sleep(5)
+# driver.find_element_by_partial_link_text('用技术打造小程序简历，让面试官耳目一新').click()
+
+# 模态框及焦点定位
+driver.get('https://www.imooc.com/')
+driver.find_elements_by_id("js-signin-btn").click()
+time.sleep(3)
+driver.find_elements_by_class_name("email").send_keys('mushishi_xu@163.com')
+element = driver.find_elements_by_class_name("password")
+element.send_keys('xu221168')
+driver.find_elements_by_class_name("moco-btn-lg").click()
+time.sleep(2)
+driver.get('https://coding.imooc.com/')
+time.sleep(3)
+
+
 driver.close()
